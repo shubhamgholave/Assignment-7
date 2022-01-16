09c218d76cf28c1957bd880874ee11c5e3c2ac82
shubhamgholave <92290517+shubhamgholave@users.noreply.github.com>
2022-01-17 00:08:20 +0530

    Update and rename README.md to assignment7.c

---
+++ assignment7.c
@@ -0,0 +1,65 @@
+# Assignment-7
+
+/*Name : Shubham Gholave
+Roll No : SYITA139*/
+
+#include <stdio.h>
+#include<stdlib.h>
+struct queue
+{
+  int size,f,r;
+  int *arr;
+};
+void enqueue(struct queue *q,int value)
+{
+  if((q->r+1)%q->size==q->f)
+  {
+    printf("queue is full\n");
+  }
+  else
+  {
+    q->r=(q->r+1)%q->size;
+    q->arr[q->r]=value;
+    printf("element added successfully\n");
+  }
+}
+void dequeue(struct queue *q)
+{
+  if(q->f==q->r)
+  {
+    printf("stack is empty\n");
+  }
+  else
+  { int value;
+    q->f=(q->f+1)%q->size;
+    value=q->arr[q->f]; 
+    printf("element deleted successfully: %d\n",value);
+  }
+}
+
+int main(void) 
+{
+struct queue q;
+q.arr=(int *)malloc(sizeof(int));
+q.size=500;
+q.f=0;
+q.r=0;
+
+enqueue(&q,5);
+enqueue(&q,10);
+enqueue(&q,15);
+enqueue(&q,20);
+enqueue(&q,25);
+enqueue(&q,30);
+enqueue(&q,35);
+printf("elements present in the array:\n");
+for(int i=1;i<=q.r;i++)
+{
+  printf("%d\n",q.arr[i]);
+}
+dequeue(&q);
+dequeue(&q);
+dequeue(&q);
+
+}
+ 
